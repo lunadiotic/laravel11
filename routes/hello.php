1907ee2f78456d42932acb02644081bbd41d0a45
aim <ahmadhakim404@gmail.com>
2024-07-10 00:34:27 +0700

    route: put naming on route

--- routes/hello.php
+++ routes/hello.php
@@ -10,3 +10,14 @@ Route::get('/hello', function () {
 Route::get('/hello/{name}/{age?}/', function ($name, $age = null) {
     return 'Hello, ' . $name . ' ' . $age;
 });
+
+
+Route::get('/foo', function () {
+    // return route('route-foo');
+
+    return redirect()->route('route-bar');
+})->name('route-foo');
+
+Route::get('/bar', function () {
+    return 'url(' . route('route-bar') . ')';
+})->name('route-bar');
