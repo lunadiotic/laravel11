6b7847b3906c86acaf443c695171466c2f5527b9
Aim <ahmad.hakim@wartek.belajar.id>
2024-07-30 00:01:58 +0700

    route: method post

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/movie', function() {
-    $movies = [];
+$movies = [];
 
-    for($i = 0; $i < 10; $i++) {
-        $movies[] = [
-            'title' => 'Movie ' . $i,
-            'year' => '2022',
-            'genre' => 'Action',
-        ];
+for($i = 0; $i < 10; $i++) {
+    $movies[] = [
+        'title' => 'Movie ' . $i,
+        'year' => '2022',
+        'genre' => 'Action',
+    ];
+}
+
+Route::get('/movie', function() use ($movies) {
+    echo '<h1>Movies</h1>';
+    echo '<ul>';
+    foreach($movies as $movie) {
+        echo '<li>' . $movie['title'] . ' - ' . $movie['year'] . ' - ' . $movie['genre'] . '</li>';
     }
+    echo '</ul>';
+});
+
+
+Route::post('/movie', function() use ($movies) {
+    $movies[] = [
+        'title' => request('title'),
+        'year' => request('year'),
+        'genre' => request('genre'),
+    ];
 
     echo '<h1>Movies</h1>';
     echo '<ul>';
