c888da4f0fbd798bb96913a230e665399997fb47
Aim <ahmad.hakim@wartek.belajar.id>
2024-07-29 23:12:05 +0700

    route: basic routing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/movie', function() {
+    $movies = [];
+
+    for($i = 0; $i < 10; $i++) {
+        $movies[] = [
+            'title' => 'Movie ' . $i,
+            'year' => '2022',
+            'genre' => 'Action',
+        ];
+    }
+
+    echo '<h1>Movies</h1>';
+    echo '<ul>';
+    foreach($movies as $movie) {
+        echo '<li>' . $movie['title'] . ' - ' . $movie['year'] . ' - ' . $movie['genre'] . '</li>';
+    }
+    echo '</ul>';
+});
