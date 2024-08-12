3104021ce4a7351f2fbc43ba3eaf8d25c96ab780
Aim <ahmad.hakim@wartek.belajar.id>
2024-08-12 14:26:02 +0700

    middleware: alias

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'isMember' => App\Http\Middleware\CheckMembership::class,
+        ]);
+
         $middleware->validateCsrfTokens(except:[
             '*'
         ]);
